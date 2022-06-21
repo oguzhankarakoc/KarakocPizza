22d75eced4eb69e06cea77949595a55dd7181a2c
oguzhankarakoc <ogzhnkrkc52@gmail.com>
2022-06-21 20:07:46 +0300

    first commit

---
+++ Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace KarakocPizza.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
